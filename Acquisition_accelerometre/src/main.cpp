f89d9b286c2b1abccaba8890f9adf5d60ef1ff99
pburlando <patrice.burlando@ac-nice.fr>
2022-11-21 22:32:25 +0100

    zzz

--- Acquisition_accelerometre/src/main.cpp
+++ Acquisition_accelerometre/src/main.cpp
@@ -4,9 +4,31 @@ sur carte micro_sd
 */
 
 #include <Arduino.h>
+#include <Wire.h>  // nécessaire pour communiquer avec la centrale inertielle par le bus I2C
+
+#define ADRESSE_MPU 0x68  // Adresse I2C MPU
+
+#define ROLL    0
+#define PITCH   1
+#define YAW     2
+
+#define X       0
+#define Y       1
+#define Z       2
+
+int accel_raw[3] = {0, 0, 0};
+int gyro_raw[3] = {0, 0, 0};
+
+int temperature = 0;
+
+
+
 
 void setup() {
   // put your setup code here, to run once:
+  Wire.begin();
+  TWBR = 12;
+  
 }
 
 void loop() {
