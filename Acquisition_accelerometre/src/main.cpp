5bc2c47a605f971115971905600ed710a64e023f
pburlando <patrice.burlando@ac-nice.fr>
2022-11-21 21:59:49 +0100

    creation

---
+++ Acquisition_accelerometre/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+Acquisition des données d'un accéléromètre
+sur carte micro_sd
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
