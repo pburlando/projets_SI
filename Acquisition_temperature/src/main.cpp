d7ced64828be0b7dec9ee81c6cc16c4ad4bff3a0
Patrice <patrice.burlando@ac-nice.fr>
2022-12-26 22:09:18 +0100

    debut de projet

---
+++ Acquisition_temperature/src/main.cpp
@@ -0,0 +1,19 @@
+/*
+DataLogger température arduino nano
+
+Enregistrement de température et hygrométrie
+capteur DHT22
+sur dataLogger Arduino Nano, carte SD et RTC DS1307
+
+*/
+
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
