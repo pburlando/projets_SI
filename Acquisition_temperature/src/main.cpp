81704583bdb8664fcbafae5173b374d21580b022
Patrice <patrice.burlando@ac-nice.fr>
2022-12-26 23:05:04 +0100

    debut de projet

--- Acquisition_temperature/src/main.cpp
+++ Acquisition_temperature/src/main.cpp
@@ -10,10 +10,41 @@ sur dataLogger Arduino Nano, carte SD et RTC DS1307
 
 #include <Arduino.h>
 
+#include <SimpleDHT.h>
+
+// for DHT22, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT22 = 10;
+SimpleDHT22 dht22(pinDHT22);
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("=================================");
+  Serial.println("Sample DHT22...");
+  
+  // read without samples.
+  // @remark We use read2 to get a float data, such as 10.1*C
+  //    if user doesn't care about the accurate data, use read to get a byte data, such as 10*C.
+  float temperature = 0;
+  float humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht22.read2(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT22 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(2000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((float)temperature); Serial.print(" *C, ");
+  Serial.print((float)humidity); Serial.println(" RH%");
+  
+  // DHT22 sampling rate is 0.5HZ.
+  delay(2500);
 }
